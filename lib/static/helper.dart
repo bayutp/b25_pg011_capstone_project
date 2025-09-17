f3682076a7680f0d5a93efb6230a98acd9d26cb0
bayu <bayuteguhpamuji@gmail.com>
2025-09-17 13:20:51 +0700

    [bayu] - add helper err msg

---
+++ lib/static/helper.dart
@@ -0,0 +1,6 @@
+class Helper{
+  static String errMsg = "Terjadi kesalahan.";
+  static String errFmt = "Data dari server tidak valid.";
+  static String errInet = "Tidak ada koneksi internet.";
+  static String errServer = "Gagal terhubung ke server. Coba lagi nanti.";
+}
