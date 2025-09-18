dced429e2b2cdef883471ea32277c02e50dcc81d
bayu <bayuteguhpamuji@gmail.com>
2025-09-18 17:27:20 +0700

    [bayu] - add shared preferences service

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:b25_pg011_capstone_project/provider/main/bottomnav_provider.dart';
+import 'package:b25_pg011_capstone_project/provider/user/user_local_provider.dart';
 import 'package:b25_pg011_capstone_project/screen/login/login_screen.dart';
 import 'package:b25_pg011_capstone_project/screen/main/main_screen.dart';
 import 'package:b25_pg011_capstone_project/screen/onboarding/onboarding_screen.dart';
 import 'package:b25_pg011_capstone_project/screen/register/register_screen.dart';
+import 'package:b25_pg011_capstone_project/service/sharedpreferences_service.dart';
 import 'package:b25_pg011_capstone_project/static/navigation_route.dart';
 import 'package:b25_pg011_capstone_project/style/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
   runApp(
     MultiProvider(
       providers: [
+        Provider(create: (context) => SharedpreferencesService(prefs)),
         ChangeNotifierProvider(create: (context) => BottomnavProvider()),
+        ChangeNotifierProvider(
+          create: (context) =>
+              UserLocalProvider(context.read<SharedpreferencesService>()),
+        ),
       ],
       child: const MyApp(),
     ),
