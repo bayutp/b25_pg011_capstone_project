4e7a7e4f5d1d806d2efa4a355563fedfead18af3
bayu <bayuteguhpamuji@gmail.com>
2025-09-20 12:42:28 +0700

    [bayu] - onboarding screen ui implementation

--- lib/screen/onboarding/onboarding_screen.dart
+++ lib/screen/onboarding/onboarding_screen.dart
@@ -1,109 +1,60 @@
+import 'package:b25_pg011_capstone_project/static/navigation_route.dart';
 import 'package:b25_pg011_capstone_project/style/colors/app_colors.dart';
-import 'package:b25_pg011_capstone_project/widget/banner_cashflow_widget.dart';
+import 'package:b25_pg011_capstone_project/widget/button_widget.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
-  // final _formKey = GlobalKey<FormState>();
-  // final _emailController = TextEditingController();
-  // final _passwordController = TextEditingController();
-
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: BannerCashflowWidget(
-                title: "Pemasukan",
-                money: 0,
-                color: AppColors.bgBlue.colors,
-                imgAssets: "assets/img/ic_in.png",
+      backgroundColor: AppColors.bgGreen.colors,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/img/ic_onboarding.png", height: 288),
+                  SizedBox.square(dimension: 50),
+                  Text(
+                    "Cerdas Catat, Cerdas Rencana",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  SizedBox.square(dimension: 20),
+                  Text(
+                    "Catat penjualan dan atur keuangan usaha agar berkembang lebih berkelanjutan.",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  SizedBox.square(dimension: 60),
+                  ButtonWidget(
+                    title: "Mulai Sekarang",
+                    textColor: AppColors.bgGreen.colors,
+                    foregroundColor: AppColors.btnGreen.colors,
+                    backgroundColor: AppColors.btnWhite.colors,
+                    onPressed: () {
+                      //todo: perlu ada pengecekan firts launch jika memang diperlukan muncul sekali saja
+                      Navigator.pushReplacementNamed(
+                        context,
+                        NavigationRoute.loginRoute.name,
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
-            Expanded(
-              child: BannerCashflowWidget(
-                title: "Pengeluaran",
-                money: 0,
-                color: AppColors.bgCream.colors,
-                imgAssets: "assets/img/ic_out.png",
-              ),
-            ),
-          ],
+          ),
         ),
       ),
-      // child: ButtonWidget(
-      //   title: "Mulai Sekarang",
-      //   textColor: AppColors.bgGreen.colors,
-      //   foregroundColor: AppColors.btnGreen.colors,
-      //   backgroundColor: AppColors.btnWhite.colors,
-      //   onPressed: () async {
-      //     await context.read<UserLocalProvider>().setStatusUser(
-      //       UserLocal(statusLogin: true),
-      //     );
-      //     ScaffoldMessenger.of(context).showSnackBar(
-      //       SnackBar(
-      //         content: Text(context.read<UserLocalProvider>().message),
-      //       ),
-      //     );
-      //   },
-      // ),
-      // child: Form(
-      //   key: _formKey,
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: [
-      //       TextFormFieldWidget(
-      //         controller: _emailController,
-      //         label: "Email",
-      //         obscureText: false,
-      //         validator: (value) {
-      //           if (value == null || value.isEmpty) {
-      //             return "Email tidak boleh kosong";
-      //           }
-      //           if (!value.contains("@")) {
-      //             return "Format email tidak valid";
-      //           }
-      //           return null;
-      //         },
-      //       ),
-      //       const SizedBox(height: 16),
-      //       TextFormFieldWidget(
-      //         controller: _passwordController,
-      //         label: "Password",
-      //         obscureText: true,
-      //         validator: (value) {
-      //           if (value == null || value.isEmpty) {
-      //             return "Password tidak boleh kosong";
-      //           }
-      //           if (value.length < 6) {
-      //             return "Password minimal 6 karakter";
-      //           }
-      //           return null;
-      //         },
-      //       ),
-      //       const SizedBox(height: 24),
-      //       ButtonWidget(
-      //         title: "Login",
-      //         textColor: AppColors.btnTextWhite.colors,
-      //         foregroundColor: AppColors.btnWhite.colors,
-      //         backgroundColor: AppColors.btnGreen.colors,
-      //         onPressed: () {
-      //           if (_formKey.currentState!.validate()) {
-      //             debugPrint(
-      //               "Login valid, email: ${_emailController.text}",
-      //             );
-      //           }
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
