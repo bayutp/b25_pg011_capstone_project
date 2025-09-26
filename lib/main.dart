a3ef8206f507c7e646b1ea702eb44cfc7b592bbc
bayutp <bayuteguhpamuji@gmail.com>
2025-09-26 15:05:28 +0700

    [bayu] - add cashflow route & transaction type provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:b25_pg011_capstone_project/data/model/user_local.dart';
+import 'package:b25_pg011_capstone_project/provider/cashflow/transaction_type_provider.dart';
 import 'package:b25_pg011_capstone_project/provider/main/bottomnav_provider.dart';
 import 'package:b25_pg011_capstone_project/provider/user/user_local_provider.dart';
+import 'package:b25_pg011_capstone_project/screen/cashflow/add/add_cashflow_screen.dart';
 import 'package:b25_pg011_capstone_project/screen/login/login_screen.dart';
 import 'package:b25_pg011_capstone_project/screen/main/main_screen.dart';
 import 'package:b25_pg011_capstone_project/screen/onboarding/onboarding_screen.dart';
@@ -24,6 +26,11 @@ void main() async {
         Provider(create: (context) => service),
         ChangeNotifierProvider(create: (context) => BottomnavProvider()),
         ChangeNotifierProvider(create: (context) => UserLocalProvider(service)),
+        ChangeNotifierProvider(
+          create: (context) =>
+              UserLocalProvider(context.read<SharedpreferencesService>()),
+        ),
+        ChangeNotifierProvider(create: (context) => TransactionTypeProvider()),
       ],
       child: MyApp(user: user),
     ),
@@ -55,13 +62,14 @@ class MyApp extends StatelessWidget {
       title: 'Capstone Project',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       initialRoute: startRoute,
       routes: {
         NavigationRoute.onboardingRoute.name: (context) => OnboardingScreen(),
         NavigationRoute.loginRoute.name: (context) => const LoginScreen(),
         NavigationRoute.registerRoute.name: (context) => const RegisterScreen(),
         NavigationRoute.homeRoute.name: (context) => const MainScreen(),
+        NavigationRoute.addCashflow.name: (context) => AddCashflowScreen(),
       },
     );
   }
