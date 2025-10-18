5be41ceab02c5339e1ab641add25d7e117737c8c
bayutp <bayuteguhpamuji@gmail.com>
2025-10-18 11:38:30 +0700

    [bayu] - fix bugs auth wrapper

--- lib/auth_wrapper.dart
+++ lib/auth_wrapper.dart
@@ -4,6 +4,8 @@ import 'package:b25_pg011_capstone_project/screen/main/main_screen.dart';
 import 'package:b25_pg011_capstone_project/screen/profile/edit_profile_screen.dart';
 import 'package:b25_pg011_capstone_project/service/auth_service.dart';
 import 'package:b25_pg011_capstone_project/service/firebase_firestore_service.dart';
+import 'package:b25_pg011_capstone_project/style/colors/app_colors.dart';
+import 'package:b25_pg011_capstone_project/widget/snackbar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:b25_pg011_capstone_project/screen/login/login_screen.dart';
@@ -54,78 +56,78 @@ class _AuthWrapperState extends State<AuthWrapper> {
           );
         }
 
-        return StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
+        if (!snapshot.hasData) return const LoginScreen();
+
+        return FutureBuilder<Widget>(
+          future: _handleAuth(snapshot.data),
+          builder: (context, asyncSnap) {
+            if (asyncSnap.connectionState == ConnectionState.waiting) {
               return const Scaffold(
                 body: Center(child: CircularProgressIndicator()),
               );
+            } else if (asyncSnap.hasError) {
+              return Scaffold(
+                body: Center(
+                  child: Text(
+                    "Terjadi kesalahan saat memuat data",
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.textError.colors,
+                    ),
+                  ),
+                ),
+              );
+            } else if (asyncSnap.hasData) {
+              return asyncSnap.data!;
+            } else {
+              return const LoginScreen();
             }
-
-            if (!snapshot.hasData) return const LoginScreen();
-
-            return FutureBuilder<Widget>(
-              future: _handleAuth(snapshot.data),
-              builder: (context, asyncSnap) {
-                if (asyncSnap.connectionState == ConnectionState.waiting) {
-                  return const Scaffold(
-                    body: Center(child: CircularProgressIndicator()),
-                  );
-                } else if (asyncSnap.hasError) {
-                  return const EditProfileScreen(newUser: true);
-                } else if (asyncSnap.hasData) {
-                  return asyncSnap.data!;
-                } else {
-                  return const LoginScreen();
-                }
-              },
-            );
           },
         );
       },
     );
   }
+}
 
-  Future<bool> setupUser(
-    AuthService service,
-    UserLocalProvider sp,
-    String uid,
-    FirebaseFirestoreService firestoreService,
-  ) async {
-    final userBuzList = await service.getUserBusiness();
-    final userBuz = userBuzList.where((buz) => buz.isActive == true).toList();
-    final fullname = await service.getFullname();
+Future<bool> setupUser(
+  AuthService service,
+  UserLocalProvider sp,
+  String uid,
+  FirebaseFirestoreService firestoreService,
+) async {
+  final userBuzList = await service.getUserBusiness();
+  final userBuz = userBuzList.where((buz) => buz.isActive == true).toList();
+  final fullname = await service.getFullname();
 
-    if (userBuz.isEmpty) return false;
+  if (userBuz.isEmpty) return false;
 
-    sp.getStatusUser();
+  sp.getStatusUser();
 
-    if (sp.userLocal == null || sp.userLocal!.idbuz.isEmpty) {
-      sp.setStatusUser(
-        UserLocal(
-          statusLogin: true,
-          statusFirstLaunch: false,
-          uid: uid,
-          idbuz: userBuz.first.idBusiness,
-          fullname: fullname,
-        ),
-      );
-    }
-
-    final today = DateTime.now();
-    final todayStr = "${today.year}-${today.month}-${today.day}";
-    final lastUpdate = sp.userLocal?.lastUpdate ?? '';
-
-    if (lastUpdate != todayStr) {
-      await firestoreService.updateExpiredTodos(
-        sp.userLocal?.idbuz ?? userBuz.first.idBusiness,
-      );
-
-      final currentUser = sp.userLocal!;
-      sp.setStatusUser(currentUser.copyWith(lastUpdate: todayStr));
-    }
-
-    return true;
+  if (sp.userLocal == null || sp.userLocal!.idbuz.isEmpty) {
+    sp.setStatusUser(
+      UserLocal(
+        statusLogin: true,
+        statusFirstLaunch: false,
+        uid: uid,
+        idbuz: userBuz.first.idBusiness,
+        fullname: fullname,
+      ),
+    );
   }
+
+  final today = DateTime.now();
+  final todayStr = "${today.year}-${today.month}-${today.day}";
+  final lastUpdate = sp.userLocal?.lastUpdate ?? '';
+
+  if (lastUpdate != todayStr) {
+    await firestoreService.updateExpiredTodos(
+      sp.userLocal?.idbuz ?? userBuz.first.idBusiness,
+    );
+
+    final currentUser = sp.userLocal!;
+    sp.setStatusUser(currentUser.copyWith(lastUpdate: todayStr));
+  }
+
+  return true;
 }
